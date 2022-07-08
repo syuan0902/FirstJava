bb65028ca286bd9045f2d54eadb78e25add9c7c2
syuan0902 <syuan7492@icloud.com>
2022-07-08 16:29:39 +0800

    commit2 complete this code

--- Java0708/com/syuan/hw01/Equation.java
+++ Java0708/com/syuan/hw01/Equation.java
@@ -1,27 +1,39 @@
 package com.syuan.hw01;
 
+import java.util.Scanner;
+
 public class Equation {
 
 	public static void main(String[] args) {
-		int a = 9 , b = 8, c = 7; //任意三值
-		int x = 0;
-		int ans;
-		//int count = a * x * x + b * x + c = 0;
-		
-		ans = (b * b) - 4 * a * c;
-		
-		if (ans > 0) { //有兩個不相等的實根
-			System.out.println(x);
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("請輸入a的值(a != 0): ");
+		int a = scanner.nextInt();
+	
+		System.out.println("請輸入b的值: ");
+		int b = scanner.nextInt();
+	
+		System.out.println("請輸入c的值: ");
+		int c = scanner.nextInt();
+	
+		scanner.close();
+	
+		//int a = 1 , b = -3, c = 2; //輸入任意三值
+		double x1 , x2; //根
+		double y = b*b - 4*a*c; //判斷式
+	
+		if (y == 0) {
+			x1 = -b / 2*a;
+			System.out.println("有兩個相等的實根: " + x1);
 		}
-		
-		else if (ans == 0) { //有兩個相等的實根
-			System.out.println(x);
-		}
-		
-		else { //沒有實根
+	
+		if (y < 0) {
 			System.out.println("沒有實根");
 		}
-
+	
+		if (y > 0) {
+			x1 = (-b + Math.sqrt(y))  / 2*a;
+			x2 = (-b - Math.sqrt(y))  / 2*a;
+			System.out.println("有兩個不相等的實根: " + x1 + "和" + x2);
+		}
 	}
-
 }
