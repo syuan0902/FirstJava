0864baff3f54dfe6d691061bad342416977c6c50
syuan7492 <syuan0902@icloud.com>
2022-07-09 17:32:16 +0800

    home push

--- Java0708/src/com/kurosaki/m0708/TryArray.java
+++ Java0708/src/com/kurosaki/m0708/TryArray.java
@@ -3,7 +3,7 @@ package com.kurosaki.m0708;
 public class TryArray {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World My Love");
 
 	}
 
